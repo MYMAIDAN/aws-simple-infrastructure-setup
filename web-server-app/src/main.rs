d55e6d1030571a01317d9f0cb8564ae910c5b76e
Mykhailo Maidan <mykhailo.maidan@gmail.com>
2023-05-14 19:00:24 +0300

    Update application by adding evn variable.

--- web-server-app/src/main.rs
+++ web-server-app/src/main.rs
@@ -1,7 +1,17 @@
+use axum::routing::get;
+use axum::Router;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let app  = Router::new()
+    .route("/simple_call",get(simple_call_handler));
     let config = aws_config::load_from_env().await; 
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service()).await.unwrap();
     println!("Hello, world!");
     Ok(())
+}
+
+async fn simple_call_handler() -> axum::response::Html<&'static str>{
+    axum::response::Html("<h1>Hello World!</h1>")
 }
